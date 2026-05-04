379eb7463b14eb0239e1100d7a7cf8bec333bab2
Universal Build <build@github.com>
2026-05-04 16:58:44 +0200

    chore: update SimpleLog cask to v1.4.3 (universal macOS)

--- Casks/simplelog.rb
+++ Casks/simplelog.rb
@@ -1,8 +1,8 @@
 cask "simplelog" do
   version "1.4.3"
-  sha256 "bf7f9ea5104fb6fc4e8de600458b0588db3f87014fff823268cc78cadf6f87ae"
+  sha256 "a83cfa60811550caf2eecb4ec209aea94f64ba530cfa399a8b8814b792795f46"
 
-  url "https://github.com/simplelogdev/simplelog/releases/download/v#{version}/SimpleLog-macOS.dmg"
+  url "https://github.com/simplelogdev/simplelog/releases/download/v#{version}/SimpleLog-macOS-universal.dmg"
   name "SimpleLog"
   desc "Fast Material Design log viewer for CloudWatch, SSH, Docker, Vercel, GCP, Azure and local files"
   homepage "https://github.com/simplelogdev/simplelog"
